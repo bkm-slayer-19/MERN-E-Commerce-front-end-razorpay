90a2110eb4eae5a99cf64bcc0cde8d92bcab9e3b
bkm-slayer-19 <itsbijayaketan@gmail.com>
2024-12-16 00:07:44 +0530

    backend port

--- src/context/AppState.jsx
+++ src/context/AppState.jsx
@@ -7,7 +7,6 @@ import "react-toastify/dist/ReactToastify.css";
 const AppState = (props) => {
   // const url = "http://localhost:1000/api";
 
-  // const url = "https://mern-e-commerce-api-youtube.onrender.com/api";
   const url = "https://mern-e-commerce-api-3.onrender.com/api";
 
 
